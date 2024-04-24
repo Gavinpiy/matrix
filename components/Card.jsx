a2d8d6c86f4e4ae6c99d87ff9402798664da01e5
Gavin Yip <gavin.yip88@gmail.com>
2024-04-24 14:35:45 +0100

    card border

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Card({ title, href, imgSrc, description }) {
   return (
     <>
-      <main className="border-2 border-black border-solid p-4 rounded-lg text-center">
+      <div className="border border-black border-solid p-4 rounded-lg text-center">
         <Link href={href} passHref>
           {title}
           <img src={imgSrc} alt={title} className="mt-2" />
         </Link>
         <p className="mt-2">{description}</p>
-      </main>
+      </div>
     </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Card from "/components/Card";
 
 export default function Home() {
   return (
-    <main className=" p-12 sm:p-18 md:p-18 lg:p-28 flex flex-col justify-center pt-18 items-center h-full sm:gap-10 md:gap-12 lg:gap-20 pt-20">
+    <main className="p-12 sm:p-18 md:p-18 lg:p-28 flex flex-col justify-center pt-18 items-center h-full sm:gap-10 md:gap-12 lg:gap-20 pt-20">
       <h1>Homepage</h1>
       <div className="flex gap-10">
         <div className="flex gap-10">
