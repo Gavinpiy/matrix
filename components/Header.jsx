c9913fef2ea831c4c8c547ad15e2633f30b15607
Gavin Yip <gavin.yip88@gmail.com>
2024-04-24 16:00:40 +0100

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,11 +3,7 @@ export default function Header() {
   return (
     <header className="bg-blue-500 text-white p-4">
       <Link href="/" title="Homepage" style={{ color: "red" }}>
-        <img
-          src="/logo.png"
-          alt="logo"
-          className="w-64 flex items-center"
-        />
+        <img src="/logo.png" alt="logo" style={{ width: 250 }} />
       </Link>
     </header>
   );
