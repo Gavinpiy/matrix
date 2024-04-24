401c8a0a575681e8e0a990459b5852859abc0b9d
Gavin Yip <gavin.yip88@gmail.com>
2024-04-24 14:31:21 +0100

    chatbot

--- src/app/2/page.js
+++ src/app/2/page.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
+//configs for chatbot
 import config from "/configs/config";
 import MessageParser from "/configs/MessageParser";
 import ActionProvider from "/configs/ActionProvider";
@@ -23,17 +24,21 @@ export default function Page() {
             actionProvider={ActionProvider}
           />
         </div>
-        <p className="max-w-80">
-          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-          and typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <div className="flex flex-col gap-10">
+          <p className="max-w-80 flex ">
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+          <p> Type in hello to activate the chatbot.</p>
+        </div>
       </div>
     </main>
   );
